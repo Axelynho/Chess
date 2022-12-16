0b64914041bebfa1d71b67ece3b90c753aca521c
Axelynho <honplus45@mail.ru>
2022-12-16 21:32:37 +0400

    added cell positions

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function addSquares(){
     console.log('addSquares');
     $('.board').html('');
     for (let coord = 0; coord<64; coord++)
-    $('.board').append(divSquare.replace('$color',isBlackSquareAt(coord) ? 'black':'white'));
+    $('.board').append(divSquare.replace('$coord',coord).replace('$color',isBlackSquareAt(coord) ? 'black':'white'));
 }
 function isBlackSquareAt(coord) {
     return(coord % 8 + Math.floor(coord / 8)) % 2;
