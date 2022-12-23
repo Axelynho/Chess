7663d1330c2f1da6efa4dc52a0f4be06101ddd36
Axelynho <honplus45@mail.ru>
2022-12-23 20:31:20 +0400

    php deleted

--- script.js
+++ script.js
@@ -1,31 +1,24 @@
 let map;
 let divSquare = '<div id ="s$coord" class="square $color"></div>';
 let divFigure = '<div id ="f$coord" class="figure"> $figure</div>';
-let isDragging = false;
 $(function(){
     start();
-    setInterval('showFiguresPHP()',3000);
 });
 function start(){
     map = new Array(64);
     addSquares();
-    showFiguresPHP();
+    showFigures('rnbqkbnrpppppppp11111111111111111111111111111111PPPPPPPPRNBQKBNR');
 }
 function setDraggable(){
-    $('.figure').draggable({
-        start: function(event,ui){
-            isDragging = true;
-        }
-    });
+    $('.figure').draggable(); 
 }
 
-function setDoppable(){
+function setDroppable(){
     $('.square').droppable({
         drop: function(event,ui){
            let fromCoord = ui.draggable.attr('id').substring(1);
            let toCoord = this.id.substring(1);
            moveFigure(fromCoord,toCoord);
-           isDragging = false;
         }
     });
 }
@@ -43,7 +36,7 @@ function addSquares(){
     $('.board').append(divSquare
         .replace('$coord',coord)
         .replace('$color',isBlackSquareAt(coord) ? 'black':'white'));
-            setDoppable();
+            setDroppable();
 }
 function showFigures(figures){
     for (let coord = 0; coord<64; coord++)
@@ -79,8 +72,4 @@ function getChessSymbole(figure) {
 }
 function isBlackSquareAt(coord) {
     return(coord % 8 + Math.floor(coord / 8)) % 2;
-}
-function showFiguresPHP(){
-    if (isDragging) return;
-    $.get('chess.php?getFigures',showFigures);
 }
